4fb70dc61b5f9b9bf8d0bd0616e13660999d3a52
lazzaric92 <lazzaric_92@hotmail.it>
2024-05-24 09:51:26 +0200

    corrected snack-1

--- snack-1/js/script.js
+++ snack-1/js/script.js
@@ -4,64 +4,15 @@
 // < [ nome del tavolo (tableName), nome dell'ospite (guestName), posto occupato (place),]
 //todo: Generiamo e stampiamo in console la lista per i segnaposto.
 
-const guests = [
-    {
-        tableName: 'Tavolo Vip',
-        guestName: 'Brad Pitt',
-        place: 1
-    },
-    {
-        tableName: 'Tavolo Vip',
-        guestName: 'Johnny Depp',
-        place: 2
-    },
-    {
-        tableName: 'Tavolo Vip',
-        guestName: 'Lady Gaga',
-        place: 3
-    },
-    {
-        tableName: 'Tavolo Vip',
-        guestName: 'Cristiano Ronaldo',
-        place: 4
-    },
-    {
-        tableName: 'Tavolo Vip',
-        guestName: 'Georgina Rodriguez',
-        place: 5
-    },
-    {
-        tableName: 'Tavolo Vip',
-        guestName: 'Chiara Ferragni',
-        place: 6
-    },
-    {
-        tableName: 'Tavolo Vip',
-        guestName: 'George Clooney',
-        place: 7
-    },
-    {
-        tableName: 'Tavolo Vip',
-        guestName: 'Amal Clooney',
-        place: 8
-    },
-    {
-        tableName: 'Tavolo Vip',
-        guestName: 'Fedez',
-        place: 9
-    },
-    {
-        tableName: 'Tavolo Vip',
-        guestName: 'Amadeus',
-        place: 10
-    },
-    {
-        tableName: 'Tavolo Vip',
-        guestName: 'Fiorello',
-        place: 11
-    }
-]
+const guests = ['Brad Pitt', 'Johnny Depp', 'Lady Gaga', 'Cristiano Ronaldo', 'Georgina Rodriguez', 'Chiara Ferragni',  'George Clooney', 'Amal Clooney', 'Fedez', 'Amadeus', 'Fiorello']
 
-guests.forEach(element => {
-    console.log(`${element.tableName} - Mr\/Ms ${element.guestName} - Place: ${element.place} `);
-});
+const guestsList = guests.map((guest, index) => {
+    const guestObj = {
+        tableName: 'Tavolo Vip',
+        guestName: guest,
+        place: index + 1
+    }
+    return guestObj;
+})
+
+console.log(guestsList);
